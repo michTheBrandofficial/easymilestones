5947645987a64c7215ad168feddc25c031129e7c
Michthebrand <michthebrand@gmail.com>
2024-11-15 09:53:58 +0100

    🚀 contract checks done

--- contracts/script/EasyMilestones.s.sol
+++ contracts/script/EasyMilestones.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.28;
 
 import { Script, console } from "forge-std/Script.sol";
 import { EasyMilestones } from "../src/EasyMilestones.sol";
@@ -7,7 +7,7 @@ import { EasyMilestones } from "../src/EasyMilestones.sol";
 contract EasyMilestonesScript is Script {
   EasyMilestones public easyMilestones;
 
-  function setUp() public { }
+  function setUp() public {}
 
   function run() public {
     vm.startBroadcast();
