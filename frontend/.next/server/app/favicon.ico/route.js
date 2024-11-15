b320897f7817f025ac4aa0185907e34d4e8154dc
Michthebrand <michthebrand@gmail.com>
2024-11-15 18:26:27 +0100

    .next deleted

--- frontend/.next/server/app/favicon.ico/route.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/favicon.ico/route.js";
-const runtime = require("../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/3279d_next_e87776._.js");
-runtime.loadChunk("server/chunks/[root of the server]__8b8dd6._.js");
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/.pnpm/next@15.0.2_@babel+core@7.26.0_react-dom@19.0.0-rc-02c0e824-20241028_react@19.0.0-rc-02c0e824_xelwhe2z5lglznkjbudtglu7em/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/favicon--route-entry.js [app-rsc] (ecmascript)\" } [app-rsc] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HXsOWQcdHh8/0iU2davBzBWkt+GmDvqMDzmtrqFoe0g=\"\n}"
