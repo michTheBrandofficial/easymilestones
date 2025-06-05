0003acb926cc310c37c619c43cbacec96edfd4ba
Mich_thedev <michthebrand@gmail.com>
2025-06-05 11:19:19 +0100

    [🚀 feat] transaction creation side branch done

--- frontend/app/screens/create-transaction/index.tsx
+++ frontend/app/screens/create-transaction/index.tsx
@@ -34,8 +34,68 @@ function CreateTransaction() {
           payments.
         </Typography>
       </div>
-      <div className="w-full flex relative z-20">
-        <Milestone />
+      <div className="w-full grid grid-cols-[20%_80%] gap-x-4 relative z-20">
+        <div className="flex flex-col">
+          {/* first milestone */}
+          <svg
+            width="36"
+            height="140"
+            viewBox="0 0 36 162"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.1172 1.37761C74.1233 55.8487 -28.5613 68.9746 10.0605 136.11"
+              stroke="#4CABEF"
+              stroke-width="2.3949"
+            />
+            <circle
+              cx="15.5556"
+              cy="146.852"
+              r="5.32201"
+              transform="rotate(92.1494 15.5556 146.852)"
+              fill="#4CABEF"
+            />
+            <circle
+              cx="15.5556"
+              cy="146.852"
+              r="13.3009"
+              transform="rotate(92.1494 15.5556 146.852)"
+              stroke="#4CABEF"
+              stroke-width="1.78225"
+            />
+          </svg>
+          {/* second milestone */}
+          <svg
+            width="36"
+            height="140"
+            viewBox="0 0 36 163"
+            fill="none"
+            className="-mt-1 ml-1 "
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M15.3683 1.83251C74.3744 56.3036 -28.3102 69.4295 10.3116 136.565"
+              stroke="#4CABEF"
+              stroke-width="2.3949"
+            />
+            <circle
+              cx="14.77"
+              cy="148.268"
+              r="5.32201"
+              transform="rotate(92.1494 14.77 148.268)"
+              fill="#4CABEF"
+            />
+            <circle
+              cx="14.7699"
+              cy="148.268"
+              r="13.3009"
+              transform="rotate(92.1494 14.7699 148.268)"
+              stroke="#4CABEF"
+              stroke-width="1.78225"
+            />
+          </svg>
+        </div>
       </div>
       <WaterBodySVG />
       <div className="w-full flex relative z-20 items-center gap-x-2 mt-auto">
