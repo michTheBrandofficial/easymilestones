d54324a8fb7d37a173881f63fadad23cbae0f15b
Mich_thedev <michthebrand@gmail.com>
2025-06-07 21:56:06 +0100

    [🚀 feat] milestone creation elements - tx_title and tx_milestones animated

--- frontend/app/screens/create-transaction/index.tsx
+++ frontend/app/screens/create-transaction/index.tsx
@@ -7,7 +7,7 @@ import {
 } from "hugeicons-react";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import PageScreen from "@/components/ui/screen";
-import { noop } from "@/lib/utils";
+import { inlineSwitch, noop } from "@/lib/utils";
 import WaterBodySVG from "../-components/water-body-svg";
 import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
@@ -35,6 +35,16 @@ function CreateTransaction() {
       lastMilestone.scrollIntoView({ behavior: "smooth", block: "end" });
     }
   }, [milestones]);
+  const txTitleRef = useRef<HTMLInputElement>(null);
+  const [txTitle, setTxTitle] = useState("");
+  useEffect(() => {
+    if (step === 1) txTitleRef.current?.focus();
+  }, [step]);
+  function handleNext() {
+    if (step === MAX_STEP) noop();
+    else setStep((p) => (p + 1) as 2);
+  }
+
   return (
     <PageScreen className="flex flex-col gap-y-4 w-full flex-grow pb-8">
       <div className="flex flex-col gap-y-3">
@@ -49,157 +59,218 @@ function CreateTransaction() {
             className="text-em-tertiary "
           />
         </Typography>
-        <Typography variant={"p"} className="text-em-text pl-1 font-medium  ">
-          Break down your transaction into milestones for secure, step-by-step
-          payments.
+        <Typography
+          variant={"p"}
+          className="text-em-text pl-1 min-h-[48px] font-medium  "
+        >
+          <AnimatePresence mode="wait">
+            {inlineSwitch(
+              step,
+              [
+                1,
+                <motion.span
+                  key={"1"}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                >
+                  {screenMessage[step]}
+                </motion.span>,
+              ],
+              [
+                2,
+                <motion.span
+                  key={"2"}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                >
+                  {screenMessage[step]}
+                </motion.span>,
+              ]
+            )}
+          </AnimatePresence>
         </Typography>
       </div>
-      <div className="w-full flex-grow max-h-[40vh] relative z-40 overflow-y-auto no-scrollbar">
-        <div className="h-full grid grid-cols-[20%_80%] gap-x-0  overflow-y-auto no-scrollbar">
-          <div className="flex flex-col ">
-            {milestones.map((_, index) => {
-              const sanePeoplesIndex = index + 1;
-              {
-                /* first milestone */
-              }
-              if (sanePeoplesIndex === 1)
-                return (
-                  <svg
-                    width="36"
-                    height="140"
-                    viewBox="0 0 36 163"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M19.9591 1.37759C-39.047 55.8487 63.6376 68.9745 25.0158 136.11"
-                      stroke="#4CABEF"
-                      stroke-width="2.3949"
-                    />
-                    <circle
-                      cx="5.32201"
-                      cy="5.32201"
-                      r="5.32201"
-                      transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 15.0396 142.695)"
-                      fill="#4CABEF"
-                    />
-                    <circle
-                      cx="14.192"
-                      cy="14.192"
-                      r="13.3009"
-                      transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 5.84314 134.164)"
-                      stroke="#4CABEF"
-                      stroke-width="1.78225"
-                    />
-                  </svg>
-                );
-              if (sanePeoplesIndex === 2)
-                return (
-                  <svg
-                    width="36"
-                    height="140"
-                    viewBox="0 0 36 163"
-                    fill="none"
-                    className="-mt-1 ml-2 "
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15.3683 1.83251C74.3744 56.3036 -28.3102 69.4295 10.3116 136.565"
-                      stroke="#4CABEF"
-                      stroke-width="2.3949"
-                    />
-                    <circle
-                      cx="14.77"
-                      cy="148.268"
-                      r="5.32201"
-                      transform="rotate(92.1494 14.77 148.268)"
-                      fill="#4CABEF"
-                    />
-                    <circle
-                      cx="14.7699"
-                      cy="148.268"
-                      r="13.3009"
-                      transform="rotate(92.1494 14.7699 148.268)"
-                      stroke="#4CABEF"
-                      stroke-width="1.78225"
-                    />
-                  </svg>
-                );
-              switch (sanePeoplesIndex % 2) {
-                case 1:
-                  return (
-                    <svg
-                      width="36"
-                      height="140"
-                      viewBox="0 0 36 163"
-                      fill="none"
-                      className="-mt-[3px] "
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M19.9591 1.37759C-39.047 55.8487 63.6376 68.9745 25.0158 136.11"
-                        stroke="#4CABEF"
-                        stroke-width="2.3949"
-                      />
-                      <circle
-                        cx="5.32201"
-                        cy="5.32201"
-                        r="5.32201"
-                        transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 15.0396 142.695)"
-                        fill="#4CABEF"
-                      />
-                      <circle
-                        cx="14.192"
-                        cy="14.192"
-                        r="13.3009"
-                        transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 5.84314 134.164)"
-                        stroke="#4CABEF"
-                        stroke-width="1.78225"
-                      />
-                    </svg>
-                  );
-                case 0:
-                  return (
-                    <svg
-                      width="36"
-                      height="140"
-                      viewBox="0 0 36 163"
-                      fill="none"
-                      className="-mt-[3px] ml-2 "
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M15.3683 1.83251C74.3744 56.3036 -28.3102 69.4295 10.3116 136.565"
-                        stroke="#4CABEF"
-                        stroke-width="2.3949"
-                      />
-                      <circle
-                        cx="14.77"
-                        cy="148.268"
-                        r="5.32201"
-                        transform="rotate(92.1494 14.77 148.268)"
-                        fill="#4CABEF"
-                      />
-                      <circle
-                        cx="14.7699"
-                        cy="148.268"
-                        r="13.3009"
-                        transform="rotate(92.1494 14.7699 148.268)"
-                        stroke="#4CABEF"
-                        stroke-width="1.78225"
-                      />
-                    </svg>
-                  );
-              }
-            })}
-          </div>
-          <div className="flex flex-col ">
-            {milestones.map((_, index) => (
-              <Milestone index={index} key={index} />
-            ))}
-          </div>
-        </div>
-      </div>
+      <AnimatePresence mode="wait">
+        {step === 1 && (
+          <motion.form
+            key={"tx_title"}
+            onSubmit={(e) => {
+              e.preventDefault()
+              if (txTitle === "") return;
+              handleNext();
+            }}
+            initial={{ opacity: 0, y: -40 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -40, transition: { delay: -0.3 } }}
+            className="w-full rounded-2xl pl-4 pr-3 py-2.5 bg-gray-400/20 backdrop-blur-[12px] flex items-center"
+          >
+            <input
+              ref={txTitleRef}
+              value={txTitle}
+              onChange={(e) => setTxTitle(e.target.value)}
+              className="w-full font-Bricolage_Grotesque font-medium text-xl bg-transparent text-em-dark focus:outline-none"
+              placeholder="Transaction title"
+            />
+            <button type="submit" className="hidden cursor-not-allowed pointer-events-none" ></button>
+          </motion.form>
+        )}
+        {step === 2 && (
+          <motion.div
+            key={"tx_milestones"}
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 40, transition: { delay: -0.3 } }}
+            className="w-full flex-grow max-h-[40vh] relative z-40 overflow-y-auto no-scrollbar"
+          >
+            <div className="h-full grid grid-cols-[20%_80%] gap-x-0  overflow-y-auto no-scrollbar">
+              <div className="flex flex-col ">
+                {milestones.map((_, index) => {
+                  const sanePeoplesIndex = index + 1;
+                  {
+                    /* first milestone */
+                  }
+                  if (sanePeoplesIndex === 1)
+                    return (
+                      <svg
+                        width="36"
+                        height="140"
+                        viewBox="0 0 36 163"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M19.9591 1.37759C-39.047 55.8487 63.6376 68.9745 25.0158 136.11"
+                          stroke="#4CABEF"
+                          stroke-width="2.3949"
+                        />
+                        <circle
+                          cx="5.32201"
+                          cy="5.32201"
+                          r="5.32201"
+                          transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 15.0396 142.695)"
+                          fill="#4CABEF"
+                        />
+                        <circle
+                          cx="14.192"
+                          cy="14.192"
+                          r="13.3009"
+                          transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 5.84314 134.164)"
+                          stroke="#4CABEF"
+                          stroke-width="1.78225"
+                        />
+                      </svg>
+                    );
+                  if (sanePeoplesIndex === 2)
+                    return (
+                      <svg
+                        width="36"
+                        height="140"
+                        viewBox="0 0 36 163"
+                        fill="none"
+                        className="-mt-1 ml-2 "
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M15.3683 1.83251C74.3744 56.3036 -28.3102 69.4295 10.3116 136.565"
+                          stroke="#4CABEF"
+                          stroke-width="2.3949"
+                        />
+                        <circle
+                          cx="14.77"
+                          cy="148.268"
+                          r="5.32201"
+                          transform="rotate(92.1494 14.77 148.268)"
+                          fill="#4CABEF"
+                        />
+                        <circle
+                          cx="14.7699"
+                          cy="148.268"
+                          r="13.3009"
+                          transform="rotate(92.1494 14.7699 148.268)"
+                          stroke="#4CABEF"
+                          stroke-width="1.78225"
+                        />
+                      </svg>
+                    );
+                  switch (sanePeoplesIndex % 2) {
+                    case 1:
+                      return (
+                        <svg
+                          width="36"
+                          height="140"
+                          viewBox="0 0 36 163"
+                          fill="none"
+                          className="-mt-[3px] "
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M19.9591 1.37759C-39.047 55.8487 63.6376 68.9745 25.0158 136.11"
+                            stroke="#4CABEF"
+                            stroke-width="2.3949"
+                          />
+                          <circle
+                            cx="5.32201"
+                            cy="5.32201"
+                            r="5.32201"
+                            transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 15.0396 142.695)"
+                            fill="#4CABEF"
+                          />
+                          <circle
+                            cx="14.192"
+                            cy="14.192"
+                            r="13.3009"
+                            transform="matrix(0.0375053 0.999296 0.999296 -0.0375053 5.84314 134.164)"
+                            stroke="#4CABEF"
+                            stroke-width="1.78225"
+                          />
+                        </svg>
+                      );
+                    case 0:
+                      return (
+                        <svg
+                          width="36"
+                          height="140"
+                          viewBox="0 0 36 163"
+                          fill="none"
+                          className="-mt-[3px] ml-2 "
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M15.3683 1.83251C74.3744 56.3036 -28.3102 69.4295 10.3116 136.565"
+                            stroke="#4CABEF"
+                            stroke-width="2.3949"
+                          />
+                          <circle
+                            cx="14.77"
+                            cy="148.268"
+                            r="5.32201"
+                            transform="rotate(92.1494 14.77 148.268)"
+                            fill="#4CABEF"
+                          />
+                          <circle
+                            cx="14.7699"
+                            cy="148.268"
+                            r="13.3009"
+                            transform="rotate(92.1494 14.7699 148.268)"
+                            stroke="#4CABEF"
+                            stroke-width="1.78225"
+                          />
+                        </svg>
+                      );
+                  }
+                })}
+              </div>
+              <div className="flex flex-col ">
+                {milestones.map((_, index) => (
+                  <Milestone index={index} key={index} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
       <WaterBodySVG />
       <div
         ref={milestoneContainerRef}
@@ -221,7 +292,7 @@ function CreateTransaction() {
                 className="inline-block"
                 initial={{ y: -40, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 40, opacity: 0 }}
+                exit={{ y: -40, opacity: 0, transition: { delay: -0.3 } }}
               >
                 Cancel
               </motion.span>
@@ -229,9 +300,9 @@ function CreateTransaction() {
               <motion.span
                 key={"back"}
                 className="inline-block"
-                initial={{ y: -40, opacity: 0 }}
+                initial={{ y: 40, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                exit={{ y: 40, opacity: 0 }}
+                exit={{ y: 40, opacity: 0, transition: { delay: -0.3 } }}
               >
                 Back
               </motion.span>
@@ -239,10 +310,8 @@ function CreateTransaction() {
           </AnimatePresence>
         </Button>
         <Button
-          onTap={() => {
-            if (step === MAX_STEP) noop();
-            else setStep((p) => (p + 1) as 2);
-          }}
+          disabled={step === 1 && txTitle === ""}
+          onTap={handleNext}
           className="w-full "
         >
           <AnimatePresence mode="wait">
@@ -250,9 +319,9 @@ function CreateTransaction() {
               <motion.span
                 key={"next"}
                 className="inline-block"
-                initial={{ y: 40, opacity: 0 }}
+                initial={{ y: -40, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                exit={{ y: -40, opacity: 0 }}
+                exit={{ y: -40, opacity: 0, transition: { delay: -0.3 } }}
               >
                 Next
               </motion.span>
@@ -262,7 +331,7 @@ function CreateTransaction() {
                 className="inline-block"
                 initial={{ y: 40, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
-                exit={{ y: -40, opacity: 0 }}
+                exit={{ y: 40, opacity: 0, transition: { delay: -0.3 } }}
               >
                 Continue
               </motion.span>
