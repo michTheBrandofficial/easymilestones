8e8f3c5ee14ab1c5acd0eb1edb81b68ec6721a70
Mich_thedev <michthebrand@gmail.com>
2025-07-01 20:43:39 +0100

    [🚀 feat] removed console statement from contracts

--- contracts/flattened.sol
+++ contracts/flattened.sol
@@ -1,7 +1,30 @@
 // SPDX-License-Identifier: GPL 3.0
 pragma solidity ^0.8.24;
 
-import { Set } from "./Libs.sol";
+// src/Libs.sol
+
+contract Set {
+  mapping(address => bool) internal addedValuesMapping;
+  address[] internal addedValuesList;
+
+  /// @dev adds an address to a list if the list doesn't have it
+  function add(address value) public {
+    if (!addedValuesMapping[value]) {
+      addedValuesMapping[value] = true;
+      addedValuesList.push(value);
+    }
+  }
+
+  function has(address value) public view returns (bool) {
+    return bool(addedValuesMapping[value]);
+  }
+
+  function toArray() public view returns (address[] memory) {
+    return addedValuesList;
+  }
+}
+
+// src/EasyMilestones.sol
 
 library LibArray {
   function last(EasyMilestones.MilestoneWithoutStatus[] memory self)
@@ -125,3 +148,4 @@ contract EasyMilestones {
     }
   }
 }
+
--- contracts/src/Libs.sol
+++ contracts/src/Libs.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL 3.0
 pragma solidity ^0.8.24;
 
-import "forge-std/console.sol";
-
 contract Set {
   mapping(address => bool) internal addedValuesMapping;
   address[] internal addedValuesList;
